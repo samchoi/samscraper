f8e16e90e2e8cf71ad37d91dc337ae051df5738d
Sam <sam.jun.choi@gmail.com>
2015-03-17 20:01:19 -0700

    fiddling around with center

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :url_check
+
   def template 
     render [self.controller_name, 'templates', params[:id]].join('/'), layout: nil 
   end
+
+  def url_check
+    #redirect_to '/' and return unless ['/guests', '/'].include?(request.path)
+  end
+
 end
